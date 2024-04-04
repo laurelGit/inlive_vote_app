ccdab54cb7f25982f2ff4eb7e0f76f41f5bc7fd0
Laurel Ndeh <laurelndeh583@gmail.com>
2024-04-04 09:32:17 +0100

    remove config

--- src/config/env.js
+++
@@ -1,7 +0,0 @@
-module.exports.env = {
-  DATABASE_URI:
-    "mongodb+srv://rootndehl:peiTZjRJzMCHbU2W@cluster0.edrlmpt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-  JWT: "788caf58433cca08571b02fd16ce4742ee17ad390517f2213bf419a55fd808af8b7659",
-};
-//mongodb+srv://rootndehl:peiTZjRJzMCHbU2W@cluster0.edrlmpt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-// mongodb+srv://rootndehl:peiTZjRJzMCHbU2W@cluster0.edrlmpt.mongodb.net/
